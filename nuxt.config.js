9df60537a9142a6e838a45f1a54913c7b3e521b0
takayukiyamaoka <tkykymok@me.com>
2021-04-17 16:54:02 +0900

    Section5を追加した

--- nuxt.config.js
+++ nuxt.config.js
@@ -35,7 +35,8 @@ export default {
   // Modules (https://go.nuxtjs.dev/config-modules)
   modules: [
     // https://go.nuxtjs.dev/axios
-    '@nuxtjs/axios'
+    '@nuxtjs/axios',
+    '@nuxt/http',
   ],
 
   // Axios module configuration (https://go.nuxtjs.dev/config-axios)
--- pages/section5/index.vue
+++ pages/section5/index.vue
@@ -1,7 +1,6 @@
 <template>
-  <!-- axios.getのサンプル + 画面遷移サンプル -->
+<!-- asyncData -->
   <main class="p-20">
-
     <div class="flex justify-center m-auto w-1/2 mb-20">
       <div class="w-full">
         <table class="w-full text-left">
@@ -25,7 +24,9 @@
               <td class="border-b border-grey-light">
                 <button
                   class="bg-green-400 p-2"
-                  @click="$router.push({ path: `section2/detail/?id=${todo.id}` })"
+                  @click="
+                    $router.push({ path: `section2/detail/?id=${todo.id}` })
+                  "
                 >
                   詳細
                 </button>
@@ -40,36 +41,21 @@
 
 <script lang="ts">
 import { Component, Vue } from 'nuxt-property-decorator';
-import { Todo } from '~/classes/todo';
-import axios from 'axios';
+import { NuxtAxiosInstance } from '@nuxtjs/axios'
 
 @Component({
-  name: 'Section2'
+  name: 'Section5',
+  async asyncData({$axios}: {$axios: NuxtAxiosInstance}) {
+    const todoList = await $axios.$get(
+      'https://jsonplaceholder.typicode.com/todos'
+    );
+    return { todoList };
+  }
 })
 export default class extends Vue {
-  public todoList: Todo[] = [];
-
-  created() {}
-
   mounted() {
-    axios.get('https://jsonplaceholder.typicode.com/todos').then(response => {
-      for (let todo of response.data) {
-        let tmpTodo = new Todo();
-        Object.assign(tmpTodo, todo);
-        this.todoList.push(tmpTodo);
-      }
-    });
+    console.log(this.$data.todoList);
   }
-
-  // async mounted () {
-  //   const { data } = await axios.get <Todo[]>('https://jsonplaceholder.typicode.com/todos');
-  //   for (let todo of data) {
-  //     let tmpTodo = new Todo()
-  //     Object.assign(tmpTodo, todo)
-  //     this.todoList.push(tmpTodo)
-  //   }
-  //   console.log(this.todoList)
-  // }
 }
 </script>
 
