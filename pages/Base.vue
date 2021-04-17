a4aeefb97bd2e0d96f9a3872da65cfca76c8df3d
takayukiyamaoka <tkykymok@me.com>
2021-04-17 13:26:12 +0900

    ベーステンプレートを追加した

---
+++ pages/Base.vue
@@ -0,0 +1,16 @@
+<template>
+  <h1>Base</h1>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'nuxt-property-decorator';
+
+@Component({
+  name: 'Base'
+})
+export default class extends Vue {
+
+}
+</script>
+
+<style></style>
